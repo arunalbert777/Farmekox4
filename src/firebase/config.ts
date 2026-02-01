2b6de78a8e70a689369ae76cee4ef6fec12357d7
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 14:42:37 +0000

    deploy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig: FirebaseOptions = {
   storageBucket: "farmekox4-94882690-a08b5.appspot.com",
   messagingSenderId: "721293130330",
   appId: "1:721293130330:web:36d26299d9394323e8b663",
-  measurementId: "G-NEWPROJECTID"
+  measurementId: ""
 };
 
 export default firebaseConfig;
