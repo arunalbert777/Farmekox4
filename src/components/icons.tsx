2186340fb9f81c994769d4c9289eec66d388475e
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 11:15:56 +0000

    not deployed i think this projet is not correctly published delete hosti

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,9 +14,10 @@ export function Sprout(props: SVGProps<SVGSVGElement>) {
         strokeLinejoin="round"
       {...props}
     >
-      <path d="M7 20h10" />
-      <path d="M10 20c5.5-16 0-16 0-16" />
-      <path d="M14 20c-5.5-16 0-16 0-16" />
+        <path d="M2 22h20"/>
+        <path d="M12 2a5 5 0 0 0-5 5v1.5c0 2.4-1.3 4.6-3.4 5.9"/>
+        <path d="M12 2a5 5 0 0 1 5 5v1.5c0 2.4 1.3 4.6 3.4 5.9"/>
+        <path d="M12 22V8"/>
     </svg>
   );
 }
