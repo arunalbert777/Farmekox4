a8a46264209b6d7bf7ad0485c2ff267eef296dcb
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 10:57:23 +0000

    i think this project is not deploying from project id changed please rec

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,7 +7,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyBleBMfdQUAExxtoOE4aU5cPKktaajNsWg",
   authDomain: "studio-3740057615-9c79a.firebaseapp.com",
   projectId: "studio-3740057615-9c79a",
-  storageBucket: "studio-3740057615-9c79a.firebasestorage.app",
+  storageBucket: "studio-3740057615-9c79a.appspot.com",
   messagingSenderId: "169592690307",
   appId: "1:169592690307:web:ce46bfc82e893d3eba2db6",
 };
