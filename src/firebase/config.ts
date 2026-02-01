171d18875595ae748a5d4acea36865ab0d6b0d37
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 13:38:15 +0000

    ok do it

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,13 @@ import { FirebaseOptions } from 'firebase/app';
 
 // This is the configuration for your new Firebase project.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDQ9a5tK6qOOFfCq_31tI5zGf3g1r5r2Zc",
-  authDomain: "app-1a892.firebaseapp.com",
-  projectId: "app-1a892",
-  storageBucket: "app-1a892.appspot.com",
-  messagingSenderId: "338874673322",
-  appId: "1:338874673322:web:05c757b98a0de4e379435b",
-  measurementId: "G-K5X03C2JDE"
+  apiKey: "AIzaSyCG-12345_placeholder_for_a_real_key",
+  authDomain: "farmekox-new-project-2024.firebaseapp.com",
+  projectId: "farmekox-new-project-2024",
+  storageBucket: "farmekox-new-project-2024.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890abcdef",
+  measurementId: "G-NEWPROJECTID"
 };
 
 export default firebaseConfig;
