4f5106706a28c4dedab5df4c17a67b4a4c00b79e
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 11:03:43 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBleBMfdQUAExxtoOE4aU5cPKktaajN

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,12 @@
 import { FirebaseOptions } from 'firebase/app';
 
-// This configuration has been re-verified for the Firebase project 'farmekox2'.
+// This configuration has been re-verified for the Firebase project 'Farmekox1'.
 // The project ID is 'studio-3740057615-9c79a'.
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyBleBMfdQUAExxtoOE4aU5cPKktaajNsWg",
   authDomain: "studio-3740057615-9c79a.firebaseapp.com",
   projectId: "studio-3740057615-9c79a",
-  storageBucket: "studio-3740057615-9c79a.appspot.com",
+  storageBucket: "studio-3740057615-9c79a.firebasestorage.app",
   messagingSenderId: "169592690307",
   appId: "1:169592690307:web:ce46bfc82e893d3eba2db6",
 };
