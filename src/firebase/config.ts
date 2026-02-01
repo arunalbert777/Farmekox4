7a28e2a6c7fc04f83cf70c89c0960dc0dfa0014d
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 14:41:14 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,12 @@ import { FirebaseOptions } from 'firebase/app';
 
 // This is the configuration for your new Firebase project.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCG-12345_placeholder_for_a_real_key",
+  apiKey: "AIzaSyCuUD99id6Z4NUQXNmQHtAID4bzx1PRyFM",
   authDomain: "farmekox4-94882690-a08b5.firebaseapp.com",
   projectId: "farmekox4-94882690-a08b5",
   storageBucket: "farmekox4-94882690-a08b5.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef",
+  messagingSenderId: "721293130330",
+  appId: "1:721293130330:web:36d26299d9394323e8b663",
   measurementId: "G-NEWPROJECTID"
 };
 
