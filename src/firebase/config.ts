31e4df7f7f1d5423c6f0396aea8454f489719815
K arun kumar Arun <arunkurubaeans@gmail.com>
2026-02-01 13:55:53 +0000

    https://studio--app-1a892.us-central1.hosted.app/     this not the corrc

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,9 +3,9 @@ import { FirebaseOptions } from 'firebase/app';
 // This is the configuration for your new Firebase project.
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCG-12345_placeholder_for_a_real_key",
-  authDomain: "farmekox-new-project-2024.firebaseapp.com",
-  projectId: "farmekox-new-project-2024",
-  storageBucket: "farmekox-new-project-2024.appspot.com",
+  authDomain: "farmekox4-94882690-a08b5.firebaseapp.com",
+  projectId: "farmekox4-94882690-a08b5",
+  storageBucket: "farmekox4-94882690-a08b5.appspot.com",
   messagingSenderId: "123456789012",
   appId: "1:123456789012:web:abcdef1234567890abcdef",
   measurementId: "G-NEWPROJECTID"
